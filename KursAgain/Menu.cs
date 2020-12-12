1d63e5ed60a31f0a7b4e4217eef95ff16bb7be80
mishka251 <mishkabelka251@gmail.com>
2020-12-13 02:17:00 +0500

    Новые формы+мелкие фиксы

---
+++ KursAgain/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace KursAgain
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Spravka spravka = new Spravka();
+            spravka.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            form.ShowDialog();
+        }
+    }
+}
---
+++ KursAgain/Spravka.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace KursAgain
+{
+    public partial class Spravka : Form
+    {
+        public Spravka()
+        {
+            InitializeComponent();
+        }
+    }
+}
